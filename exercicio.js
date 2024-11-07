e3bddadd74d3532dd6c0cb59b5335e20ab5e45b7
Matias Scherer Lunkes <mslunkes@outlook.com>
2024-11-06 21:02:17 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+function areaDoRetangulo(a, b) {
+    return a * b;
+}
+console.log(areaDoRetangulo(12, 15));
+function saudacao(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+console.log(saudacao("Joao"));
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+function areaDoRetangulo (a: number, b: number):number {
+    return a * b
+}
+console.log(areaDoRetangulo(12, 15))
+
+function saudacao(nome: string):string {
+    return `Olá ${nome}` 
+}
+console.log(saudacao("Joao"))
